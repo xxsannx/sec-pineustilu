d3d58a8256c433dd30cbd4322ba146a636403afa
RizqyNurfauzella <fauzella171@gmail.com>
2025-12-06 10:43:04 +0700

    Partials Pedoman Page

---
+++ resources/views/partials/pedoman/informasi.blade.php
@@ -0,0 +1,13 @@
+<section class="max-w-3xl mx-auto text-[#0b5a3e] px-4 md:px-0">
+    <h4 class="italic text-[#017249] font-extrabold text-lg mb-4">Informasi</h4>
+    <ul class="list-disc list-inside marker:text-[#017249] space-y-3 text-base text-gray-700 mb-6">
+        <li>Kelebihan pembayaran/pengembalian dana akan ditransfer paling lambat 14 hari kerja setelah data rekening
+            penerima diberikan.</li>
+        <li>Pengembalian dana akibat reservasi gagal akan diproses paling lambat 3 hari kerja setelah data rekening
+            penerima diberikan.</li>
+        <li>Jika terjadi keadaan di luar kendali (misalnya pembatasan pemerintah/lockdown yang menyebabkan area
+            ditutup), pengembalian dana dikenakan potongan 50%.</li>
+    </ul>
+    <p class="text-center italic text-gray-600">Dalam semua kasus lain, syarat &amp; ketentuan berlaku sebagaimana
+        tertulis</p>
+</section>
