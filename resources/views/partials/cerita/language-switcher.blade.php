65c4cc7624e4cade16435b900c52f6cdaba38d62
RizqyNurfauzella <fauzella171@gmail.com>
2025-12-06 10:41:48 +0700

    Partials Cerita Page

---
+++ resources/views/partials/cerita/language-switcher.blade.php
@@ -0,0 +1,5 @@
+<div class="lang-switcher" role="tablist" aria-label="Choose language">
+    <button role="tab" aria-pressed="false" data-lang="id">Indonesia</button>
+    <button role="tab" aria-pressed="false" data-lang="ja">日本語</button>
+    <button role="tab" aria-pressed="false" data-lang="en">English</button>
+</div>
