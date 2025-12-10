8868b7662a5b0b07464c0a53f57e5f26c3d7bce9
RizqyNurfauzella <fauzella171@gmail.com>
2025-12-11 01:44:39 +0700

    Add CSS untuk Pedoman Section, Fixing Layout Syarat Ketentuan, & Refactor Header

--- resources/views/pedoman.blade.php
+++ resources/views/pedoman.blade.php
@@ -5,7 +5,9 @@
 @section('content')
 
     <main class="pt-24 w-full max-w-screen-xl mx-auto px-6 pb-28 bg-[#f6fbf8]">
-        @include('partials.pedoman.header')
+        <header class="text-center py-10">
+            <h1 class="text-4xl md:text-5xl font-extrabold text-[#017249] tracking-wider">SYARAT & KETENTUAN</h1>
+        </header>
 
         @include('partials.pedoman.syarat-ketentuan')
         @include('partials.pedoman.divider')
