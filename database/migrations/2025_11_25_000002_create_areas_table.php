eecdc5fde831f7c26f2dfcbfe8bf7702757c7fe7
Muhammad Ihsan Firjatulloh Al Khasaf <muhihsanfa@gmail.com>
2025-12-13 21:51:19 +0700

    Perubahan Urutan Migration

--- database/migrations/2025_11_25_000002_create_areas_table.php
+++ database/migrations/2025_11_25_000002_create_areas_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('areas', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('slug')->unique();
             $table->text('description')->nullable();
             $table->decimal('extra_charge_full', 15, 2)->default(0);
             $table->decimal('extra_charge_breakfast', 15, 2)->default(0);
